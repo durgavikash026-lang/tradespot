b9a0f15bd5440fa531d481da0d4c5f102aa03335
Vikash <durgavikash026@gmail.com>
2026-05-21 00:26:46 +0530

    Fixed login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import { useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase";
 
-export default function SignupPage() {
+export default function LoginPage() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const auth = getAuth(app);
 
-  const handleSignup = async () => {
+  const handleLogin = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      alert("Signup Successful");
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Login Successful");
     } catch (error) {
-      alert("Error creating account");
+      alert("Invalid Email or Password");
     }
   };
 
@@ -26,7 +26,7 @@ export default function SignupPage() {
       <div className="bg-gray-900 p-10 rounded-3xl w-[350px] shadow-2xl">
 
         <h1 className="text-3xl font-bold mb-6 text-center text-green-400">
-          Signup
+          Login
         </h1>
 
         <input
@@ -44,10 +44,10 @@ export default function SignupPage() {
         />
 
         <button
-          onClick={handleSignup}
+          onClick={handleLogin}
           className="w-full bg-green-500 hover:bg-green-600 text-black py-3 rounded-xl font-bold"
         >
-          Signup
+          Login
         </button>
 
       </div>
